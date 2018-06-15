d37827ee8cf969d0a043b30eb08fa920d82b7605
y905699146 <905699146@qq.com>
2018-06-15 18:35:03 +0800

    修改数字解析

--- leptjson.c
+++ leptjson.c
@@ -1,8 +1,12 @@
 #include<assert.h>
 #include<stdlib.h>
+#include<math.h>
+#include<errno.h>
 #include"leptjson.h"
 
 #define EXPECT(c,ch) do{ assert(*c->json==(ch)); c->json++; }while(0)
+#define ISDIGIT(ch) ((ch)>='0'&&(ch)<='9')
+#define ISDIGIT1TO9 ((ch)>='1'&&(ch)<='9')
 
 typedef struct{
 	const char* json;
@@ -23,16 +27,63 @@ int lept_parse(lept_value* v,const char* json)
 	}
 	return ret;
 }
+static int lept_parse_literal(lept_context* c,lept_value* v,const char* literal,lept_type type)
+{
+	size_t i;
+	EXPECT(c,literal[0]);
+	for(i=0;literal[i+1];i++)
+		if (c->json[i]!=literal[i+1])
+		{
+			return LEPT_PARSE_INVALID_VALUE;
+		}
+	c->json +=i;
+	v->type=type;
+	return LEPT_PARSE_OK;
+}
+
+static int lept_parse_number(lept_context* c,lept_value* v)
+{
+	const char* p=c->json;
+	if(*p=='-')   p++;
+	if(*p=='0')   p++;
+	else
+	{
+		if(!ISDIGIT1TO9(*p))   return LEPT_PARSE_INVALID_VALUE;
+		for (p++;ISDIGIT(*p) ;p++ );
+	}
+	if(*p=='.')
+	{
+		p++;
+		if(!ISDIGIT(*p))
+			return LEPT_PARSE_INVALID_VALUE;
+		for (p++;ISDIGIT(*p) ;p++ );
+	}
+	if(*p=='e'||*p=='E')
+	{
+		p++;
+		if(*p=='+'||*p=='-')
+			p++;
+		if(!ISDIGIT(*p))       return LEPT_PARSE_INVALID_VALUE;
+		for (p++;ISDIGIT(*p) ;p++ );
+	}
+	errno=0;
+	v->n=strtod(c->json,NULL);
+	if (errno == ERANGE && (v->n == HUGE_VAL || v->n == -HUGE_VAL))
+        return LEPT_PARSE_NUMBER_TOO_BIG;
+    v->type = LEPT_NUMBER;
+    c->json = p;
+    return LEPT_PARSE_OK;
+} 
 
 static int lept_parse_value(lept_context* c,lept_value* v)
 {
 	switch(*c->json)
 	{
-		case 'n':   return lept_parse_null(c,v);
-		case '\0':  return LEPT_PARSE_EXPECT_VALUE;
-		case 't':  return lept_parse_true(c, v);
-        case 'f':  return lept_parse_false(c, v);
-		default:    return lept_parse_number(c,v);
+		case 't':  return lept_parse_literal(c, v, "true", LEPT_TRUE);
+        case 'f':  return lept_parse_literal(c, v, "false", LEPT_FALSE);
+        case 'n':  return lept_parse_literal(c, v, "null", LEPT_NULL);
+        default:   return lept_parse_number(c, v);
+        case '\0': return LEPT_PARSE_EXPECT_VALUE;
 	}
 }
 /*≈–∂œ «∑Ò «null*/
