aea86190e8148afc65f82f9e1d067a3b8de319df
y905699146 <905699146@qq.com>
2018-06-25 15:51:55 +0800

    tornado_tcptest

---
+++ tornado_tcptest.py
@@ -0,0 +1,21 @@
+#tcp_server.py
+from tornado import ioloop,gen,iostream
+from tornado.tcpserver import TCPServer
+class MyTcpServer(TCPServer):
+    @gen.coroutine
+    def handle_stream(self,stream,address):
+        try:
+            while True:
+                msg=yield stream.read_bytes(20,partial=True)
+                print(msg," from ",address)
+                yield stream.write(msg[::-1])
+                if msg=='over':
+                    stream.close()
+        except iostream.StreamClosedError:
+            pass
+
+if __name__=='__main__':
+    server=MyTcpServer()
+    server.listen(8760)
+    server.start()
+    ioloop.IOLoop.current().start()
